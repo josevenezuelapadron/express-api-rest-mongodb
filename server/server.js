c4a0d281f68130af6b03c676580983087580839f
José Padrón <jose.padron712@gmail.com>
2019-02-15 12:04:18 -0400

    peticiones get, post, put, delete

--- server/server.js
+++ server/server.js
@@ -2,8 +2,35 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.json({name: "José"});
+const bodyParser = require("body-parser");
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+// parse application/json
+app.use(bodyParser.json());
+
+
+app.get("/usuario", (req, res) => {
+  res.json("get Usuario");
+});
+
+app.post("/usuario", (req, res) => {
+  const body = req.body;
+
+  res.json(body);
+});
+
+app.put("/usuario/:id", (req, res) => {
+  const id = req.params.id;
+
+  res.json({
+    id
+  });
+});
+
+app.delete("/usuario", (req, res) => {
+  res.json("delete Usuario");
 });
 
 app.listen(port, () => console.log(`Server running in port: ${port}`));
