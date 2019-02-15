33a9801e7e18d8704398dd05c1938e692b2e0c7c
José Padrón <jose.padron712@gmail.com>
2019-02-15 11:49:46 -0400

    Inicio del proyecto

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({name: "José"});
+});
+
+app.listen(port, () => console.log(`Server running in port: ${port}`));
