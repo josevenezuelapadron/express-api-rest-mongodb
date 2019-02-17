5d77a70c827d110bcc9fc281ab1ef7200dae65c3
José Padrón <jose.padron712@gmail.com>
2019-02-17 16:01:58 -0400

    variable entorno mongo

--- server/config/configu.js
+++ server/config/configu.js
@@ -7,7 +7,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
   urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-  urlDB = 'mongodb://darioxlz:28462837x*@cluster0-shard-00-00-rif5r.mongodb.net:27017,cluster0-shard-00-01-rif5r.mongodb.net:27017,cluster0-shard-00-02-rif5r.mongodb.net:27017/cafe?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
